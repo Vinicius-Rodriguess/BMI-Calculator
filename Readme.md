631339bd90b1551c1a4b3f11229a51b5793e0b07
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2024-08-05 13:52:32 -0300

    React

--- Readme.md
+++ Readme.md
@@ -1,5 +1,8 @@
-# BMI Calculator
-### This calculates your Body Mass Index (BMI).
-<a href="https://vinicius-rodriguess.github.io/BMI-Calculator/" target="_blank">Click here to test the project!</a>
-<p></p>
-<img src="./src/img/page.png"/>
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/components/ImcCalc.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import "./ImcCalc.css"
+import Button from "./Button.jsx"
+
+const ImcCalc = ({calcImc}) => {
+
+  const [height, setHeight] = useState("")
+  const [weight, setWeight] = useState("")
+
+  const clearForm = e => {
+    e.preventDefault()
+    setHeight("")
+    setWeight("")
+  }
+
+  const validDigits = text => {
+    return text.replace(/[^0-9,]/g, "")
+  }
+
+  const handleHeightChange = e => {
+    const updatedValue = validDigits(e.target.value)
+    setHeight(updatedValue)
+  }
+
+  const handleWeightChange = e => {
+    const updatedValue = validDigits(e.target.value)
+    setWeight(updatedValue)
+  }
+
+  return (
+    <div id="calc-container">
+      <h2>Calculadora de IMC</h2>
+      <form id="imc-form">
+        <div className="form-inputs">
+          <div className="form-control">
+            <label htmlFor="height">Altura:</label>
+            <input type="text" name="height" id="height" placeholder="Exemplo 1,75" onChange={e => handleHeightChange(e)} value={height}/>
+          </div>
+          <div className="form-control">
+            <label htmlFor="weight">Peso:</label>
+            <input type="text" name="weight" id="weight" placeholder="Exemplo 1,75" onChange={e => handleWeightChange(e)} value={weight}/>
+          </div>
+          <div className="action-control">
+            <Button id="calc-btn" text="Calcular" action={(e) => calcImc(e, height, weight)}/>
+            <Button id="clear-btn" text="Limpar" action={clearForm}/>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ImcCalc
